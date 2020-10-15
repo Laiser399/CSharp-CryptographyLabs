c174f8437383d2f182678d00f8ee22f8a3db1cb1
Laiser399 <Laiser399@mail.ru>
2020-10-15 20:55:35 +0300

    added TryParse with ulong

---
+++ CryptographyLabs/Extended.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyLabs
+{
+    static class Extended
+    {
+        public static bool TryParse(string strValue, out uint value)
+        {
+            strValue = strValue.Replace(" ", "");
+
+            if (strValue.Length > 2 && strValue.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    value = Convert.ToUInt32(strValue.Substring(2), 16);
+                    return true;
+                }
+                catch
+                {
+                    value = 0;
+                    return false;
+                }
+            }
+            else if (strValue.Length > 2 && strValue.StartsWith("0b", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    value = Convert.ToUInt32(strValue.Substring(2), 2);
+                    return true;
+                }
+                catch
+                {
+                    value = 0;
+                    return false;
+                }
+            }
+            else
+            {
+                return uint.TryParse(strValue, out value);
+            }
+        }
+
+        public static bool TryParse(string strValue, out ulong value)
+        {
+            strValue = strValue.Replace(" ", "");
+
+            if (strValue.Length > 2 && strValue.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    value = Convert.ToUInt64(strValue.Substring(2), 16);
+                    return true;
+                }
+                catch
+                {
+                    value = 0;
+                    return false;
+                }
+            }
+            else if (strValue.Length > 2 && strValue.StartsWith("0b", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    value = Convert.ToUInt64(strValue.Substring(2), 2);
+                    return true;
+                }
+                catch
+                {
+                    value = 0;
+                    return false;
+                }
+            }
+            else
+            {
+                return ulong.TryParse(strValue, out value);
+            }
+        }
+    }
+}
