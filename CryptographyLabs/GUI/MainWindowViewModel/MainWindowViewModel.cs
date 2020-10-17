9c8f84f1b4679de1b1e254169b545161ff5027a2
Laiser399 <Laiser399@mail.ru>
2020-10-17 14:03:27 +0300

    Encrypt\Decrypt interface template done

--- CryptographyLabs/GUI/MainWindowViewModel/MainWindowViewModel.cs
+++ CryptographyLabs/GUI/MainWindowViewModel/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +45,73 @@ namespace CryptographyLabs.GUI
 
         private Task8ViewModel _task8VM;
         public Task8ViewModel Task8VM => _task8VM ?? (_task8VM = new Task8ViewModel());
+
+
+
+        private ObservableCollection<CryptoItemViewModel> _items;
+        public ObservableCollection<CryptoItemViewModel> Items =>
+            _items ?? (_items = new ObservableCollection<CryptoItemViewModel>());
+
+        private ObservableCollection<string> _col1, _col2;
+        public ObservableCollection<string> Col1 => _col1 ?? (_col1 = new ObservableCollection<string>());
+        public ObservableCollection<string> Col2 => _col2 ?? (_col2 = new ObservableCollection<string>());
+
+
+        public MainWindowViewModel()
+        {
+            Random random = new Random(123);
+            for (int i = 0; i < 40; ++i)
+                Items.Add(new CryptoItemViewModel { Filename=$"/{random.Next()}", CryptoProgress = random.NextDouble() * 100 });
+
+            Col1.Add("col1");
+            Col2.Add("col2");
+        }
+
+    }
+
+    class CryptoItemViewModel : BaseViewModel
+    {
+        private long _startTime = DateTime.Now.Ticks;
+        public long StartTime => _startTime;
+
+        private string _filename = "";
+        public string Filename
+        {
+            get => _filename;
+            set
+            {
+                _filename = value;
+                NotifyPropertyChanged(nameof(Filename));
+            }
+        }
+
+        private string _statusString = "aga";
+        public string StatusString
+        {
+            get => _statusString;
+            set
+            {
+                _statusString = value;
+                NotifyPropertyChanged(nameof(StatusString));
+            }
+        }
+
+        private double _cryptoProgress = 0;
+        public double CryptoProgress
+        {
+            get => _cryptoProgress;
+            set
+            {
+                if (value > 100)
+                    _cryptoProgress = 100;
+                else if (value < 0)
+                    _cryptoProgress = 0;
+                else
+                    _cryptoProgress = value;
+                NotifyPropertyChanged(nameof(CryptoProgress));
+            }
+        }
+
 
     }
 }
