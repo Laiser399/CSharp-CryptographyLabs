ad7ffa2d629790f85b6608cb3527e03f9278a6cc
Laiser399 <Laiser399@mail.ru>
2020-10-17 21:21:39 +0300

    vernam gui done

---
+++ CryptographyLabs/GUI/MainWindowViewModel/CryptoViewModels/VernamCryptoViewModel.cs
@@ -0,0 +1,146 @@
+﻿using Crypto;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyLabs.GUI
+{
+    class VernamCryptoViewModel : BaseViewModel
+    {
+        private bool _isEncrypt = true;
+        public bool IsEncrypt
+        {
+            get => _isEncrypt;
+            set
+            {
+                _isEncrypt = value;
+                NotifyPropertyChanged(nameof(IsEncrypt));
+            }
+        }
+
+        private string _filename = "";
+        public string Filename
+        {
+            get => _filename;
+            set
+            {
+                _filename = value;
+                NotifyPropertyChanged(nameof(Filename));
+            }
+        }
+
+        private string _keyFilename = "";
+        public string KeyFilename
+        {
+            get => _keyFilename;
+            set
+            {
+                _keyFilename = value;
+                NotifyPropertyChanged(nameof(KeyFilename));
+            }
+        }
+
+        private bool _isDeleteFileAfter = false;
+        public bool IsDeleteFileAfter
+        {
+            get => _isDeleteFileAfter;
+            set
+            {
+                _isDeleteFileAfter = value;
+                NotifyPropertyChanged(nameof(IsDeleteFileAfter));
+            }
+        }
+
+        private ObservableCollection<CryptoProgressViewModel> _progressViewModels;
+        public ObservableCollection<CryptoProgressViewModel> ProgressViewModels =>
+            _progressViewModels ?? (_progressViewModels = new ObservableCollection<CryptoProgressViewModel>());
+
+        private RelayCommand _changeFilenameCommand;
+        public RelayCommand ChangeFilenameCommand =>
+            _changeFilenameCommand ?? (_changeFilenameCommand = new RelayCommand(_ => ChangeFilename()));
+
+        private RelayCommand _changeKeyFilenameCommand;
+        public RelayCommand ChangeKeyFilenameCommand =>
+            _changeKeyFilenameCommand ?? (_changeKeyFilenameCommand = new RelayCommand(_ => ChangeKeyFilename()));
+
+        private RelayCommand _goCommand;
+        public RelayCommand GoCommand =>
+            _goCommand ?? (_goCommand = new RelayCommand(_ => Go()));
+
+        private void ChangeFilename()
+        {
+            using (var dialog = new CommonOpenFileDialog())
+            {
+                if (!IsEncrypt)
+                    dialog.Filters.Add(new CommonFileDialogFilter("Encrypted file", ".v399"));
+                dialog.Filters.Add(new CommonFileDialogFilter("Any file", "*"));
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                    Filename = dialog.FileName;
+            }
+        }
+
+        private void ChangeKeyFilename()
+        {
+            using (var dialog = new CommonOpenFileDialog())
+            {
+                dialog.Filters.Add(new CommonFileDialogFilter("Key file", ".vkey399"));
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                    KeyFilename = dialog.FileName;
+            }
+        }
+
+        private void Go()
+        {
+            var viewModel = new CryptoProgressViewModel
+            {
+                CryptoName = "Vernam",
+                Filename = Filename
+            };
+            ProgressViewModels.Add(viewModel);
+
+            Task task0;
+            if (IsEncrypt)
+            {
+                task0 = Vernam.EncryptFileAsync(Filename, progress => viewModel.CryptoProgress = progress);
+                viewModel.StatusString = "Encrypting";
+            }
+            else
+            {
+                task0 = Vernam.DecryptFileAsync(Filename, KeyFilename,
+                    progress => viewModel.CryptoProgress = progress);
+                viewModel.StatusString = "Decrypting";
+            }
+            
+
+            task0.ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    viewModel.StatusString = "Error: " + task.Exception.InnerException.Message;
+                }
+                else if (IsDeleteFileAfter)
+                {
+                    viewModel.StatusString = "Deleting file";
+                    try
+                    {
+                        File.Delete(Filename);
+                        viewModel.StatusString = "Done successfully";
+                    }
+                    catch (Exception e)
+                    {
+                        viewModel.StatusString = "Error: " + e.Message;
+                    }
+                }
+                else
+                    viewModel.StatusString = "Done successfully";
+
+                viewModel.IsDone = true;
+            });
+        }
+    }
+}
--- CryptographyLabs/Vernam.cs
+++ CryptographyLabs/Vernam.cs
@@ -8,6 +8,134 @@ using System.IO;
 
 namespace Crypto
 {
+    public static class Vernam
+    {
+
+        public static void EncryptFile(string path)
+        {
+            EncryptFile(path, 0);
+        }
+
+        public static Task EncryptFileAsync(string path, Action<double> progressCallback = null)
+        {
+            return Task.Run(() =>
+            {
+                EncryptFile(path, 100000, progressCallback);
+            });
+        }
+
+        private static void EncryptFile(string path, int bufSize, Action<double> progressCallback = null)
+        {
+            string encryptPath = path + ".v399";
+            string keyPath = path + ".vkey399";
+            long bytesCount = new FileInfo(path).Length;
+
+            if (progressCallback is null)
+                GenerateKeyFile(keyPath, bytesCount);
+            else
+                GenerateKeyFile(keyPath, bytesCount, progress => progressCallback(progress / 2));
+
+            using (FileStream inStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (FileStream inKeyStream = new FileStream(keyPath, FileMode.Open, FileAccess.Read))
+            using (FileStream outStream = new FileStream(encryptPath, FileMode.OpenOrCreate, FileAccess.Write))
+            using (CryptoStream outCrypto = new CryptoStream(outStream, new VernamCryptoTransform(inKeyStream), CryptoStreamMode.Write))
+            {
+                progressCallback?.Invoke(0.5);
+                if (bufSize <= 0)
+                {
+                    inStream.CopyTo(outCrypto);
+                    progressCallback?.Invoke(1);
+                }
+                else
+                {
+                    byte[] buf = new byte[bufSize];
+                    for (long i = 0; i < bytesCount;)
+                    {
+                        int hasRead = inStream.Read(buf, 0, buf.Length);
+                        outCrypto.Write(buf, 0, hasRead);
+                        i += hasRead;
+                        progressCallback?.Invoke((double)i / bytesCount / 2 + 0.5);
+                    }
+                }
+            }
+        }
+
+        private static void GenerateKeyFile(string path, long bytesCount, Action<double> progressCallback = null)
+        {
+            using (FileStream outStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                Random random = new Random((int)DateTime.Now.Ticks);
+                int bufSize = 80000;
+                byte[] buff = new byte[bufSize];
+                for (long i = 0; i < bytesCount;)
+                {
+                    random.NextBytes(buff);
+                    int toWrite = (int)Math.Min(bufSize, bytesCount - i);
+                    outStream.Write(buff, 0, toWrite);
+                    i += toWrite;
+                    progressCallback?.Invoke((double)i / bytesCount);
+                }
+            }
+        }
+
+        public static void DecryptFile(string encryptedPath, string keyPath, string decryptPath = null)
+        {
+            DecryptFile(encryptedPath, keyPath, decryptPath, 0, null);
+        }
+
+        public static Task DecryptFileAsync(string encryptedPath, string keyPath,
+            Action<double> progressCallback = null, string decryptPath = null)
+        {
+            return Task.Run(() =>
+            {
+                DecryptFile(encryptedPath, keyPath, decryptPath, 100000, progressCallback);
+            });
+        }
+
+        private static void DecryptFile(string encryptedPath, string keyPath, string decryptPath, 
+            int bufSize, Action<double> progressCallback = null)
+        {
+            if (decryptPath is null)
+            {
+                if (encryptedPath.EndsWith(".v399"))
+                {
+                    decryptPath = encryptedPath.Substring(0, encryptedPath.Length - 5);
+                }
+                else
+                {
+                    string dirName = Path.GetDirectoryName(encryptedPath);
+                    decryptPath = Path.Combine(dirName, "decrypted");
+                }
+            }
+
+            using (FileStream inStream = new FileStream(encryptedPath, FileMode.Open, FileAccess.Read))
+            using (FileStream inKeyStream = new FileStream(keyPath, FileMode.Open, FileAccess.Read))
+            using (FileStream outStream = new FileStream(decryptPath, FileMode.OpenOrCreate, FileAccess.Write))
+            using (CryptoStream outCrypto = new CryptoStream(outStream, new VernamCryptoTransform(inKeyStream), CryptoStreamMode.Write))
+            {
+                progressCallback?.Invoke(0);
+                if (bufSize <= 0)
+                {
+                    inStream.CopyTo(outCrypto);
+                    progressCallback?.Invoke(1);
+                }
+                else
+                {
+                    long bytesCount = inStream.Length;
+                    byte[] buf = new byte[bufSize];
+                    for (long i = 0; i < bytesCount;)
+                    {
+                        int hasRead = inStream.Read(buf, 0, buf.Length);
+                        outCrypto.Write(buf, 0, hasRead);
+                        i += hasRead;
+                        progressCallback?.Invoke((double)i / bytesCount);
+                    }
+                }
+            }
+        }
+        
+    }
+
     public class VernamCryptoTransform : ICryptoTransform
     {
         public int InputBlockSize => 1;
