5b99bb7e47d1631a4851d35ab99875f58a93a820
Sergey Semenov <Laiser399@mail.ru>
2022-09-25 13:50:03 +0300

    Add realisation of BigIntegerCalculationService with unit tests

---
+++ Module.RSA.UnitTests/BigIntegerCalculationServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Numerics;
+using Module.RSA.Services;
+using Module.RSA.Services.Abstract;
+using NUnit.Framework;
+
+namespace Module.RSA.UnitTests;
+
+[TestFixture]
+public class BigIntegerCalculationServiceTests
+{
+    private IBigIntegerCalculationService? _bigIntegerCalculationService;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _bigIntegerCalculationService = new BigIntegerCalculationService();
+    }
+
+    [Test]
+    [TestCase(0, 2, 3, 0)]
+    [TestCase(1, 20, 3, 1)]
+    [TestCase(2, 19, 7, 2)]
+    [TestCase(7, 120, 13, 1)]
+    public void BinPowMod_Test(int value, int pow, int mod, int expected)
+    {
+        var actual = _bigIntegerCalculationService!.BinPowMod(value, pow, mod);
+        Assert.AreEqual((BigInteger)expected, actual);
+    }
+
+    [Test]
+    [TestCase(1, 0, 1)]
+    [TestCase(2, 1, 1)]
+    [TestCase(13, 0, 13)]
+    [TestCase(1024, 10, 1)]
+    [TestCase(392, 3, 49)]
+    public void Factor2Out_CommonTest(int value, int expectedExponent2, int expectedRemainder)
+    {
+        _bigIntegerCalculationService!.Factor2Out(value, out var actualExponent2, out var actualRemainder);
+
+        Assert.AreEqual(expectedExponent2, actualExponent2);
+        Assert.AreEqual((BigInteger)expectedRemainder, actualRemainder);
+    }
+
+    [Test, Timeout(1000)]
+    public void Factor2Out_InvalidArgumentTest()
+    {
+        Assert.Throws<ArgumentException>(
+            () => _bigIntegerCalculationService!.Factor2Out(0, out _, out _)
+        );
+
+        Assert.Throws<ArgumentException>(
+            () => _bigIntegerCalculationService!.Factor2Out(-3, out _, out _)
+        );
+    }
+
+    [Test]
+    [TestCase(1, 1, 1)]
+    [TestCase(1, 3, 1)]
+    [TestCase(3, 1, 1)]
+    [TestCase(49, 7, 7)]
+    [TestCase(7, 49, 7)]
+    [TestCase(14, 21, 7)]
+    [TestCase(1024, 1024, 1024)]
+    public void GreatestCommonDivisor_Test(int a, int b, int expected)
+    {
+        var actual = _bigIntegerCalculationService!.GreatestCommonDivisor(a, b);
+
+        Assert.AreEqual((BigInteger)expected, actual);
+    }
+}
---
+++ Module.RSA/Services/BigIntegerCalculationService.cs
@@ -0,0 +1,75 @@
+﻿using System.Numerics;
+using Module.RSA.Services.Abstract;
+
+namespace Module.RSA.Services;
+
+public class BigIntegerCalculationService : IBigIntegerCalculationService
+{
+    public BigInteger BinPowMod(BigInteger value, BigInteger pow, BigInteger mod)
+    {
+        if (value == 0 && pow == 0)
+            throw new ArgumentException("value = 0 and pow = 0. That's all.");
+        if (pow < 0)
+            throw new ArgumentOutOfRangeException(nameof(pow), "Pow must be >= 0.");
+        if (mod < 1)
+            throw new ArgumentOutOfRangeException(nameof(mod), "Mod must be > 0.");
+
+        var multiplier = value % mod;
+        if (multiplier < 0)
+        {
+            multiplier += mod;
+        }
+
+        BigInteger result = 1;
+        while (pow > 0)
+        {
+            if ((pow & 1) == 1)
+            {
+                result = (result * multiplier) % mod;
+            }
+
+            multiplier = (multiplier * multiplier) % mod;
+            pow >>= 1;
+        }
+
+        return result;
+    }
+
+    public void Factor2Out(BigInteger value, out int exponent2, out BigInteger remainder)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("Value must be positive.", nameof(value));
+        }
+
+        exponent2 = 0;
+        remainder = value;
+        while ((remainder & 1) == 0)
+        {
+            ++exponent2;
+            remainder >>= 1;
+        }
+    }
+
+    public BigInteger GreatestCommonDivisor(BigInteger a, BigInteger b)
+    {
+        if (a <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(a), "Value must be > 0.");
+        }
+
+        if (b <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(b), "Value must be > 0.");
+        }
+
+        while (b > 0)
+        {
+            var tm = a;
+            a = b;
+            b = tm % b;
+        }
+
+        return a;
+    }
+}
