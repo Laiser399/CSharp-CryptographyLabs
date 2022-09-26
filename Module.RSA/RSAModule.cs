5471f1473444a2bdb79b897a73b0d699a0d7f91c
Sergey Semenov <Laiser399@mail.ru>
2022-09-26 23:12:16 +0300

    DI in RSAModule

---
+++ Module.RSA/RSAModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Module.RSA.Services;
+using Module.RSA.Services.Abstract;
+
+namespace Module.RSA;
+
+public class RSAModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<PrimesPairGenerator>()
+            .As<IPrimesPairGenerator>();
+        builder
+            .RegisterType<MillerRabinPrimalityTester>()
+            .As<IPrimalityTester>()
+            .SingleInstance();
+        builder
+            .RegisterType<RoundCountCalculator>()
+            .As<IRoundCountCalculator>()
+            .SingleInstance();
+        builder
+            .RegisterType<BigIntegerCalculationService>()
+            .As<IBigIntegerCalculationService>()
+            .SingleInstance();
+        builder
+            .RegisterType<RandomBigIntegerGenerator>()
+            .As<IRandomBigIntegerGenerator>()
+            .SingleInstance();
+    }
+}
