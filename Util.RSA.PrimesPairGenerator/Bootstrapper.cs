d9a09e4d7b7eadf83d77b7dc4359b88196563373
Sergey Semenov <Laiser399@mail.ru>
2022-09-27 21:42:10 +0300

    Add first realisation of Util.RSA.PrimesPairGenerator

---
+++ Util.RSA.PrimesPairGenerator/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Module.RSA;
+
+namespace Util.RSA.PrimesPairGenerator;
+
+public static class Bootstrapper
+{
+    public static ILifetimeScope BuildLifetimeScope()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterModule<RSAModule>();
+        return builder.Build();
+    }
+}
--- Util.RSA.PrimesPairGenerator/Program.cs
+++ Util.RSA.PrimesPairGenerator/Program.cs
@@ -1,3 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using System.Numerics;
+using Autofac;
+using Module.RSA.Entities.Abstract;
+using Module.RSA.Services.Abstract;
+using Util.RSA.PrimesPairGenerator;
+using Util.RSA.PrimesPairGenerator.Entities;
+
+var lifetimeScope = Bootstrapper.BuildLifetimeScope();
+
+var parameters = new GeneratorParameters
+{
+    Random = new Random(),
+    ByteCount = 256,
+    PQDifferenceMinBitCount = 256 * 8 - 1,
+    AddingTriesCount = 100,
+    PrimalityProbability = 0.995
+};
+
+GeneratePQ(lifetimeScope, parameters, out var p, out var q);
+
+const string saveDirectoryName = "PrimePairs";
+
+if (!Directory.Exists(saveDirectoryName))
+{
+    Directory.CreateDirectory(saveDirectoryName);
+}
+
+var timeStr = DateTime.Now.ToString("yyyy.MM.dd HH-mm-ss.fff");
+File.WriteAllText(Path.Combine(saveDirectoryName, $"{timeStr} p.txt"), p.ToString());
+File.WriteAllText(Path.Combine(saveDirectoryName, $"{timeStr} q.txt"), q.ToString());
+Console.WriteLine("Result saved");
+
+
+static void GeneratePQ(ILifetimeScope lifetimeScope, GeneratorParameters parameters, out BigInteger p, out BigInteger q)
+{
+    using var scope = RegisterParameters(lifetimeScope, parameters);
+    var generator = scope.Resolve<IPrimesPairGenerator>();
+
+    Console.WriteLine("Start generation...");
+    var stopwatch = Stopwatch.StartNew();
+    generator.Generate(out p, out q);
+    stopwatch.Stop();
+    Console.WriteLine($"Generation done in {stopwatch.Elapsed}");
+}
+
+
+static ILifetimeScope RegisterParameters(ILifetimeScope scope, GeneratorParameters parameters)
+{
+    return scope.BeginLifetimeScope(builder =>
+    {
+        builder
+            .Register(_ => parameters)
+            .As<IRandomProvider>()
+            .As<IPrimesPairGeneratorParameters>()
+            .As<IPrimalityTesterParameters>()
+            .SingleInstance();
+    });
+}
