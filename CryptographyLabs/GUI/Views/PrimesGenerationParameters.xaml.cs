f6ca8e9190c11f59975bc4679534a3f8300e0fee
Sergey Semenov <Laiser399@mail.ru>
2022-10-03 21:11:12 +0300

    Add view PrimesGenerationParameters

---
+++ CryptographyLabs/GUI/Views/PrimesGenerationParameters.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class PrimesGenerationParameters : Grid
+{
+    public PrimesGenerationParameters()
+    {
+        InitializeComponent();
+    }
+}
