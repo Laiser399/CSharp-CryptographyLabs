6e241b3bab370f1b7bad93c62b84ab1ffbdecba5
Sergey Semenov <Laiser399@mail.ru>
2022-10-03 21:30:47 +0300

    Add design time view model for PrimesGenerationParameters; Add bindings to PrimesGenerationParameters.

---
+++ CryptographyLabs/GUI/AbstractViewModels/IPrimesGenerationParametersVM.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IPrimesGenerationParametersVM
+{
+    int Seed { get; set; }
+    int ByteCount { get; set; }
+    double Probability { get; set; }
+
+    bool IsSaveToFile { get; set; }
+    string SaveDirectory { get; set; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/PrimesGenerationParametersDTVM.cs
@@ -0,0 +1,13 @@
+﻿using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class PrimesGenerationParametersDTVM : IPrimesGenerationParametersVM
+{
+    public int Seed { get; set; } = 666;
+    public int ByteCount { get; set; } = 256;
+    public double Probability { get; set; } = 0.995;
+
+    public bool IsSaveToFile { get; set; } = true;
+    public string SaveDirectory { get; set; } = @"Path\To\Some\Directory";
+}
