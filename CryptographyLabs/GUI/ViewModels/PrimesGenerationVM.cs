33e6b2cf5c68033a0af8b5b41871d384f44b6a0e
Sergey Semenov <Laiser399@mail.ru>
2022-10-03 22:16:57 +0300

    Add draft implementation of PrimesGenerationVM

---
+++ CryptographyLabs/GUI/ViewModels/PrimesGenerationVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.ViewModels;
+
+public class PrimesGenerationVM : IPrimesGenerationVM
+{
+    public IPrimesGenerationParametersVM Parameters { get; }
+    public IPrimesGenerationResultsVM Results { get; }
+
+    public ICommand Generate => _generate ??= new RelayCommand(_ => Generate_Internal());
+    private ICommand? _generate;
+
+    public PrimesGenerationVM(
+        IPrimesGenerationParametersVM parametersVM,
+        IPrimesGenerationResultsVM resultsVM)
+    {
+        Parameters = parametersVM;
+        Results = resultsVM;
+    }
+
+    private void Generate_Internal()
+    {
+        throw new NotImplementedException();
+    }
+}
