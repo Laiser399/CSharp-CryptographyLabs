bbf89eba30bbccf648f376967d5d70d4dc9fc6c1
Sergey Semenov <Laiser399@mail.ru>
2022-10-03 21:37:00 +0300

    Add design time view model for PrimesGenerationResults; Add bindings to PrimesGenerationResults.

---
+++ CryptographyLabs/GUI/AbstractViewModels/IPrimesGenerationResultsVM.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IPrimesGenerationResultsVM
+{
+    BigInteger P { get; }
+    BigInteger Q { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/PrimesGenerationResultsDTVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class PrimesGenerationResultsDTVM : IPrimesGenerationResultsVM
+{
+    public BigInteger P => 3;
+    public BigInteger Q { get; } = BigInteger.Parse("29083476023984576234756234985734501781265987659376510");
+}
