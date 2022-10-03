8ce5a47df735dd9ddc0075fceab1ad21ecc720f0
Sergey Semenov <Laiser399@mail.ru>
2022-10-03 21:45:03 +0300

    Add design time view model for PrimesGeneration; Add bindings to PrimesGeneration.

---
+++ CryptographyLabs/GUI/AbstractViewModels/IPrimesGenerationVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IPrimesGenerationVM
+{
+    IPrimesGenerationParametersVM Parameters { get; }
+    IPrimesGenerationResultsVM Results { get; }
+
+    ICommand Generate { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/PrimesGenerationDTVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class PrimesGenerationDTVM : IPrimesGenerationVM
+{
+    public IPrimesGenerationParametersVM Parameters { get; } = new PrimesGenerationParametersDTVM();
+    public IPrimesGenerationResultsVM Results { get; } = new PrimesGenerationResultsDTVM();
+
+    public ICommand Generate { get; } = new RelayCommand(_ => { });
+}
