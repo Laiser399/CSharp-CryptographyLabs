64b4e7f10cc8b36749f507f7df463d5edbe13de3
Sergey Semenov <Laiser399@mail.ru>
2022-10-05 20:30:44 +0300

    Add validator for IRSAKeyGenerationParametersVM

---
+++ CryptographyLabs/GUI/Validators/RSAKeyGenerationParametersVMValidator.cs
@@ -0,0 +1,19 @@
+﻿using CryptographyLabs.GUI.AbstractViewModels;
+using FluentValidation;
+
+namespace CryptographyLabs.GUI.Validators;
+
+public class RSAKeyGenerationParametersVMValidator : AbstractValidator<IRSAKeyGenerationParametersVM>
+{
+    public RSAKeyGenerationParametersVMValidator()
+    {
+        RuleFor(x => x.P)
+            .NotNull()
+            .GreaterThanOrEqualTo(2)
+            .OverridePropertyName(nameof(IRSAKeyGenerationParametersVM.PStr));
+        RuleFor(x => x.Q)
+            .NotNull()
+            .GreaterThanOrEqualTo(2)
+            .OverridePropertyName(nameof(IRSAKeyGenerationParametersVM.QStr));
+    }
+}
