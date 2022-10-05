7ee045d5d2516dda1c655657963d7c5ababd8d9e
Sergey Semenov <Laiser399@mail.ru>
2022-10-05 20:17:03 +0300

    Add views for RSA key generation

---
+++ CryptographyLabs/GUI/Views/RSAKeyGenerationParameters.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class RSAKeyGenerationParameters : Grid
+{
+    public RSAKeyGenerationParameters()
+    {
+        InitializeComponent();
+    }
+}
