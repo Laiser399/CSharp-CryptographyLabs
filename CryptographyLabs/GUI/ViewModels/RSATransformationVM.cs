248c8a3d0255a8a778e1c09630b9505b64c8799c
Sergey Semenov <Laiser399@mail.ru>
2022-10-07 23:39:37 +0300

    Add realisation of RSATransformationVM

--- CryptographyLabs/GUI/ViewModels/RSATransformationVM.cs
+++ CryptographyLabs/GUI/ViewModels/RSATransformationVM.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Windows;
 using System.Windows.Input;
 using CryptographyLabs.GUI.AbstractViewModels;
+using Module.RSA.Entities;
+using Module.RSA.Services.Abstract;
 
 namespace CryptographyLabs.GUI.ViewModels;
 
@@ -14,13 +18,115 @@ public class RSATransformationVM : IRSATransformationVM
     public ICommand Transform => _transform ??= new RelayCommand(_ => Transform_Internal());
     private ICommand? _transform;
 
-    public RSATransformationVM(IRSATransformationParametersVM parametersVM)
+    private readonly IRSATransformService _rsaTransformService;
+
+    public RSATransformationVM(
+        IRSATransformationParametersVM parametersVM,
+        IRSATransformService rsaTransformService)
     {
         Parameters = parametersVM;
+        _rsaTransformService = rsaTransformService;
     }
 
     private void Transform_Internal()
     {
-        throw new NotImplementedException();
+        if (IsInProgress)
+        {
+            return;
+        }
+
+        if (Parameters.HasErrors)
+        {
+            MessageBox.Show("Parameters is not configured correctly.");
+            return;
+        }
+
+        if (!File.Exists(Parameters.FilePath))
+        {
+            MessageBox.Show($"File \"{Parameters.FilePath}\" does not exists.");
+            return;
+        }
+
+        IsInProgress = true;
+
+        try
+        {
+            if (!TryReadInputFile(out var data))
+            {
+                return;
+            }
+
+            if (data.Length == 0)
+            {
+                MessageBox.Show("Input file is empty.");
+                return;
+            }
+
+            var key = new RSAKey(Parameters.Exponent!.Value, Parameters.Modulus!.Value);
+
+            var transformedData = Parameters.IsEncryption
+                ? _rsaTransformService.Encrypt(data, key, x => Progress = x)
+                : _rsaTransformService.Decrypt(data, key, x => Progress = x);
+
+            SaveResult(transformedData);
+        }
+        finally
+        {
+            IsInProgress = false;
+        }
+    }
+
+    private bool TryReadInputFile(out byte[] data)
+    {
+        try
+        {
+            data = File.ReadAllBytes(Parameters.FilePath);
+            return true;
+        }
+        catch (IOException e)
+        {
+            MessageBox.Show($"IO error on reading input file.\n\n{e}");
+            data = null!;
+            return false;
+        }
+        catch (SystemException e)
+        {
+            MessageBox.Show($"System error on reading input file.\n\n{e}");
+            data = null!;
+            return false;
+        }
+    }
+
+    private void SaveResult(byte[] transformedData)
+    {
+        var saveFilePath = GetSaveFilePath();
+        try
+        {
+            File.WriteAllBytes(saveFilePath, transformedData);
+        }
+        catch (IOException e)
+        {
+            MessageBox.Show($"Error on save result to file \"{saveFilePath}\".\n\n{e}");
+        }
+        catch (SystemException e)
+        {
+            MessageBox.Show($"Error on save result to file \"{saveFilePath}\".\n\n{e}");
+        }
+    }
+
+    private string GetSaveFilePath()
+    {
+        if (Parameters.IsEncryption)
+        {
+            return Parameters.FilePath + ".bin";
+        }
+
+        var extension = Path.GetExtension(Parameters.FilePath).ToLower();
+        if (extension == ".bin")
+        {
+            return Path.GetFileNameWithoutExtension(Parameters.FilePath);
+        }
+
+        return Parameters.FilePath + ".decrypted";
     }
 }
