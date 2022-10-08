cff1ae73d76f572e777151767b0f9ab3cd97dfd9
Sergey Semenov <Laiser399@mail.ru>
2022-10-08 22:33:17 +0300

    Add validator for RSA factorization parameters view model

---
+++ CryptographyLabs/GUI/Validators/RSAFactorizationAttackParametersVMValidator.cs
@@ -0,0 +1,15 @@
+﻿using CryptographyLabs.GUI.AbstractViewModels;
+using FluentValidation;
+
+namespace CryptographyLabs.GUI.Validators;
+
+public class RSAFactorizationAttackParametersVMValidator : AbstractValidator<IRSAFactorizationAttackParametersVM>
+{
+    public RSAFactorizationAttackParametersVMValidator()
+    {
+        RuleFor(x => x.Modulus)
+            .NotNull()
+            .GreaterThan(2)
+            .OverridePropertyName(nameof(IRSAFactorizationAttackParametersVM.ModulusStr));
+    }
+}
