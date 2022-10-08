638eee03b1ff3abfe4fffec9f55d0a16002f142c
Sergey Semenov <Laiser399@mail.ru>
2022-10-08 21:46:30 +0300

    Add views for RSA factorization attack

---
+++ CryptographyLabs/GUI/Views/RSAFactorizationAttackResults.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class RSAFactorizationAttackResults : Grid
+{
+    public RSAFactorizationAttackResults()
+    {
+        InitializeComponent();
+    }
+}
