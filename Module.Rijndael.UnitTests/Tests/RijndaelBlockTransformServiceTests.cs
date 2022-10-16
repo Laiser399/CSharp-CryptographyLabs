1b805e834621a66f21c68e577ed853c085ae5d7f
Sergey Semenov <Laiser399@mail.ru>
2022-10-16 12:47:24 +0300

    Add Rijndael block transform interface; Add unit tests for it.

---
+++ Module.Rijndael.UnitTests/Tests/RijndaelBlockTransformServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Module.Rijndael.Services.Abstract;
+using NUnit.Framework;
+
+namespace Module.Rijndael.UnitTests.Tests;
+
+[TestFixture]
+public class RijndaelBlockTransformServiceTests
+{
+    private IRijndaelBlockTransformService? _rijndaelBlockTransformService;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _rijndaelBlockTransformService = null;
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Transform_Test()
+    {
+        var random = new Random(123);
+
+        var text = new byte[16];
+        var encrypted = new byte[16];
+        var decrypted = new byte[16];
+        for (var i = 0; i < 1000; i++)
+        {
+            random.NextBytes(text);
+            _rijndaelBlockTransformService!.Encrypt(text, encrypted);
+            _rijndaelBlockTransformService!.Decrypt(text, decrypted);
+
+            CollectionAssert.AreNotEqual(text, encrypted);
+            CollectionAssert.AreEqual(text, decrypted);
+        }
+    }
+
+    [Test]
+    public void Transform_InvalidArgumentTest()
+    {
+        Assert.Throws<ArgumentException>(
+            () => _rijndaelBlockTransformService!.Encrypt(new byte[15], new byte[16])
+        );
+        Assert.Throws<ArgumentException>(
+            () => _rijndaelBlockTransformService!.Encrypt(new byte[16], new byte[15])
+        );
+    }
+}
---
+++ Module.Rijndael/Services/Abstract/IRijndaelBlockTransformService.cs
@@ -0,0 +1,8 @@
+﻿namespace Module.Rijndael.Services.Abstract;
+
+public interface IRijndaelBlockTransformService
+{
+    void Encrypt(Span<byte> input, Span<byte> output);
+
+    void Decrypt(Span<byte> input, Span<byte> output);
+}
