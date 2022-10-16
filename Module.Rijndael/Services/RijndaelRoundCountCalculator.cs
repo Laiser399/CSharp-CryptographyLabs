2afcbe0d726e3845a8fbef383890f4fb90358458
Sergey Semenov <Laiser399@mail.ru>
2022-10-16 19:05:42 +0300

    Add implementation of Rijndael round count calculator

---
+++ Module.Rijndael/Services/RijndaelRoundCountCalculator.cs
@@ -0,0 +1,22 @@
+﻿using Module.Rijndael.Enums;
+using Module.Rijndael.Services.Abstract;
+
+namespace Module.Rijndael.Services;
+
+public class RijndaelRoundCountCalculator : IRijndaelRoundCountCalculator
+{
+    public int GetRoundCount(RijndaelSize blockSize, RijndaelSize keySize)
+    {
+        if (blockSize == RijndaelSize.S128 && keySize == RijndaelSize.S128)
+        {
+            return 10;
+        }
+
+        if (blockSize == RijndaelSize.S256 || keySize == RijndaelSize.S256)
+        {
+            return 14;
+        }
+
+        return 12;
+    }
+}
