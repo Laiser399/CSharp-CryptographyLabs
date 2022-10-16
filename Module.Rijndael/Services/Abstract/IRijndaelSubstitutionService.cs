581897820636e1b83290d71afd58d31eb9a1f930
Sergey Semenov <Laiser399@mail.ru>
2022-10-16 15:54:01 +0300

    Move substitution logic to separate service

---
+++ Module.Rijndael/Services/Abstract/IRijndaelSubstitutionService.cs
@@ -0,0 +1,8 @@
+﻿namespace Module.Rijndael.Services.Abstract;
+
+public interface IRijndaelSubstitutionService
+{
+    void SubstituteBytes(Span<byte> state);
+
+    void SubstituteBytesInversed(Span<byte> state);
+}
