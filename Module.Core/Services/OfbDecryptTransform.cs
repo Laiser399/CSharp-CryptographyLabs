3c0661082b5a3eb1b7c37e0516e494be49fc82a8
Sergey Semenov <Laiser399@mail.ru>
2022-10-17 19:11:27 +0300

    Add empty CBC, CFB, OFB transforms

---
+++ Module.Core/Services/OfbDecryptTransform.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace Module.Core.Services;
+
+public class OfbDecryptTransform : ICryptoTransform
+{
+    public bool CanReuseTransform { get; }
+    public bool CanTransformMultipleBlocks { get; }
+    public int InputBlockSize { get; }
+    public int OutputBlockSize { get; }
+
+    public int TransformBlock(
+        byte[] inputBuffer,
+        int inputOffset,
+        int inputCount,
+        byte[] outputBuffer,
+        int outputOffset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
