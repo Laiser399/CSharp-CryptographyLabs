e51a406881aeac05d2fe315c19fed375b7e0230a
Sergey Semenov <Laiser399@mail.ru>
2022-10-17 22:31:47 +0300

    Add DI module in Module.Core

---
+++ Module.Core/CoreModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Module.Core.Services;
+using Module.Core.Services.Abstract;
+
+namespace Module.Core;
+
+public class CoreModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<XorService>()
+            .As<IXorService>()
+            .SingleInstance();
+    }
+}
