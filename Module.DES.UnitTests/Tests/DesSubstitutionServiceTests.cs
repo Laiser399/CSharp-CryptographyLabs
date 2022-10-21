016b094026548d210800245ef2567e74785ff317
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 02:32:58 +0300

    Add DES substitution service test

---
+++ Module.DES.UnitTests/Tests/DesSubstitutionServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Module.DES.Services.Abstract;
+using NUnit.Framework;
+
+namespace Module.DES.UnitTests.Tests;
+
+[TestFixture]
+public class DesSubstitutionServiceTests
+{
+    private IDesSubstitutionService? _desSubstitutionService;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _desSubstitutionService = null;
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    [TestCase(
+        0b11100101_00001100_01111001_01011011_00000110_10110110ul,
+        ExpectedResult = 0b00110011_10111010_01011011_00000111u
+    )]
+    public uint Test(ulong value48)
+    {
+        return _desSubstitutionService!.Substitute(value48);
+    }
+}
---
+++ Module.DES/Services/Abstract/IDesSubstitutionService.cs
@@ -0,0 +1,6 @@
+﻿namespace Module.DES.Services.Abstract;
+
+public interface IDesSubstitutionService
+{
+    uint Substitute(ulong value48);
+}
