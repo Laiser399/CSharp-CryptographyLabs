fd335ac7016cc66ac09bc67896797ee189862185
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 17:19:43 +0300

    Add base tests class for block transform

---
+++ Module.Core.UnitTests/Tests/BlockCryptoTransformBaseTests.cs
@@ -0,0 +1,82 @@
+﻿using Module.Core.Enums;
+using Module.Core.Factories.Abstract;
+using NUnit.Framework;
+
+namespace Module.Core.UnitTests.Tests;
+
+[TestFixture]
+public abstract class BlockCryptoTransformBaseTests<T>
+{
+    private IBlockCryptoTransformFactory<T>? _blockCryptoTransformFactory;
+
+    private Random? _random;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _blockCryptoTransformFactory = GetBlockCryptoTransformFactory();
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _random = new Random(123);
+    }
+
+    protected abstract IBlockCryptoTransformFactory<T> GetBlockCryptoTransformFactory();
+
+    protected void TestTransform(T parameters)
+    {
+        var blockEncryptTransform = _blockCryptoTransformFactory!.Create(TransformDirection.Encrypt, parameters);
+        var blockDecryptTransform = _blockCryptoTransformFactory!.Create(TransformDirection.Decrypt, parameters);
+
+        var data = new byte[blockEncryptTransform.InputBlockSize];
+        var encrypted = new byte[blockEncryptTransform.OutputBlockSize];
+        var decrypted = new byte[blockDecryptTransform.OutputBlockSize];
+
+        for (var i = 0; i < 1000; i++)
+        {
+            _random!.NextBytes(data);
+            blockEncryptTransform.Transform(data, encrypted);
+            blockDecryptTransform.Transform(encrypted, decrypted);
+
+            CollectionAssert.AreNotEqual(data, encrypted);
+            CollectionAssert.AreEqual(data, decrypted);
+        }
+    }
+
+    [Test]
+    public void Transform_InvalidArgumentTest()
+    {
+        var parameters = GetDefaultParameters();
+        var blockEncryptTransform = _blockCryptoTransformFactory!.Create(TransformDirection.Encrypt, parameters);
+        var blockDecryptTransform = _blockCryptoTransformFactory!.Create(TransformDirection.Decrypt, parameters);
+
+        Assert.Throws<ArgumentException>(
+            () => blockEncryptTransform.Transform(
+                new byte[blockEncryptTransform.InputBlockSize - 1],
+                new byte[blockEncryptTransform.OutputBlockSize]
+            )
+        );
+        Assert.Throws<ArgumentException>(
+            () => blockEncryptTransform.Transform(
+                new byte[blockEncryptTransform.InputBlockSize],
+                new byte[blockEncryptTransform.OutputBlockSize - 1]
+            )
+        );
+        Assert.Throws<ArgumentException>(
+            () => blockDecryptTransform.Transform(
+                new byte[blockDecryptTransform.InputBlockSize - 1],
+                new byte[blockDecryptTransform.OutputBlockSize]
+            )
+        );
+        Assert.Throws<ArgumentException>(
+            () => blockDecryptTransform.Transform(
+                new byte[blockDecryptTransform.InputBlockSize],
+                new byte[blockDecryptTransform.OutputBlockSize - 1]
+            )
+        );
+    }
+
+    protected abstract T GetDefaultParameters();
+}
---
+++ Module.Rijndael.UnitTests/Tests/RijndaelBlockCryptoTransformTests.cs
@@ -0,0 +1,101 @@
+﻿using Autofac;
+using Module.Core;
+using Module.Core.Factories.Abstract;
+using Module.Core.UnitTests.Tests;
+using Module.Rijndael.Entities;
+using Module.Rijndael.Entities.Abstract;
+using Module.Rijndael.Enums;
+using NUnit.Framework;
+
+namespace Module.Rijndael.UnitTests.Tests;
+
+[TestFixture]
+public class RijndaelBlockCryptoTransformTests : BlockCryptoTransformBaseTests<IRijndaelParameters>
+{
+    private readonly IContainer _container;
+
+    public RijndaelBlockCryptoTransformTests()
+    {
+        _container = BuildContainer();
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetTransformTestCases))]
+    public void Transform_Test(IRijndaelParameters parameters)
+    {
+        TestTransform(parameters);
+    }
+
+    private static IReadOnlyCollection<IRijndaelParameters> GetTransformTestCases()
+    {
+        var keysBytes = new[]
+        {
+            new byte[]
+            {
+                220, 98, 68, 222,
+                197, 55, 127, 233,
+                53, 242, 68, 32,
+                91, 119, 175, 95
+            },
+            new byte[]
+            {
+                93, 230, 245, 204, 216, 129,
+                66, 132, 92, 63, 19, 244,
+                227, 157, 177, 22, 49, 56,
+                50, 142, 121, 181, 247, 18
+            },
+            new byte[]
+            {
+                130, 237, 235, 12, 213, 170, 49, 57,
+                87, 227, 107, 115, 51, 119, 87, 130,
+                246, 189, 57, 255, 113, 65, 74, 72,
+                244, 66, 191, 162, 9, 233, 234, 1
+            }
+        };
+
+        var blockSizes = new[]
+        {
+            RijndaelSize.S128,
+            RijndaelSize.S192,
+            RijndaelSize.S256
+        };
+
+        var testCases =
+            from keyBytes in keysBytes
+            from blockSize in blockSizes
+            select new RijndaelParameters(keyBytes, blockSize);
+
+        return testCases.ToList();
+    }
+
+    protected override IBlockCryptoTransformFactory<IRijndaelParameters> GetBlockCryptoTransformFactory()
+    {
+        return _container.Resolve<IBlockCryptoTransformFactory<IRijndaelParameters>>();
+    }
+
+    protected override IRijndaelParameters GetDefaultParameters()
+    {
+        var keyBytes = new byte[]
+        {
+            220, 98, 68, 222,
+            197, 55, 127, 233,
+            53, 242, 68, 32,
+            91, 119, 175, 95
+        };
+        var blockSize = RijndaelSize.S128;
+        return new RijndaelParameters(keyBytes, blockSize);
+    }
+
+    private static IContainer BuildContainer()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterModule<CoreModule>();
+        builder.RegisterModule(new RijndaelModule
+        {
+            UseDefaultGaloisFieldConfiguration = true
+        });
+
+        return builder.Build();
+    }
+}
