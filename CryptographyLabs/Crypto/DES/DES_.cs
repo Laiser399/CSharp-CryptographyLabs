edead5d9c776e0d70c9f5617788df53f8bbd726c
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 19:09:33 +0300

    Remove DES legacy code

---
+++ CryptographyLabs/Crypto/DES/DES_.cs
@@ -0,0 +1,15 @@
+﻿namespace CryptographyLabs.Crypto
+{
+    public static class DES_
+    {
+        public const int BlockSize = 8;
+
+        public enum Mode
+        {
+            ECB = 0,
+            CBC = 1,
+            CFB = 2,
+            OFB = 3
+        }
+    }
+}
