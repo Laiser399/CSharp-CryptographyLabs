3cd18300a287e418012f2e9ee6357ce36641cdd5
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 18:48:06 +0300

    Move permutation networks in separate project

--- Module.PermutationNetwork/Networks/PermutationNetwork64.cs
+++ Module.PermutationNetwork/Networks/PermutationNetwork64.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using CryptographyLabs;
-
-namespace ConsoleTests
+﻿namespace Module.PermutationNetwork.Networks
 {
-    class Swapper32 : PermutationNetwork32
+    class Swapper64 : PermutationNetwork64
     {
-        internal Swapper32(byte[] permutation, int phase)
+        internal Swapper64(byte[] permutation, int phase)
         {
-            Masks.Add((permutation[0] == 1) ? 1u << phase : 0);
+            Masks.Add((permutation[0] == 1) ? 1ul << phase : 0);
         }
     }
 
-    public class PermutationNetwork32
+    public class PermutationNetwork64
     {
         // The list of masks for the final permutation
-        public List<uint> Masks { get; set; }
+        public List<ulong> Masks { get; set; }
 
-        protected PermutationNetwork32()
+        protected PermutationNetwork64()
         {
-            Masks = new List<uint>();
+            Masks = new List<ulong>();
         }
 
-        public PermutationNetwork32(byte[] permutation) : this()
+        public PermutationNetwork64(byte[] permutation) : this()
         {
             if (!CheckPermutation(permutation))
             {
@@ -34,70 +30,69 @@ namespace ConsoleTests
 
         private static bool CheckPermutation(byte[] permutation)
         {
-            if (permutation.Length != 32)
+            if (permutation.Length != 64)
             {
                 return false;
             }
 
-            int check = 0;
-            for (var i = 0; i < 32; i++)
+            long check = 0;
+            for (int i = 0; i < 64; i++)
             {
+#pragma warning disable CS0675 // Битовая операция или оператор, использовавшийся в операнде с расширением знака
                 check |= 1 << i;
+#pragma warning restore CS0675 // Битовая операция или оператор, использовавшийся в операнде с расширением знака
             }
 
             return check == -1;
         }
 
-        private PermutationNetwork32(byte[] permutation, int phaseParm) : this()
+        private PermutationNetwork64(byte[] permutation, int phaseParm) : this()
         {
             Initialize(permutation, phaseParm);
         }
 
         private void Initialize(byte[] permutation, int phase)
         {
-            PermutationNetwork32 permutationNetworkEven;
-            PermutationNetwork32 permutationNetworkOdd;
-            var size = permutation.Length;
-            var delta = 32 / size;
+            PermutationNetwork64 permutationNetworkEven;
+            PermutationNetwork64 permutationNetworkOdd;
+            int size = permutation.Length;
+            int delta = 64 / size;
+            ulong inputMask;
+            ulong outputMask;
 
             // Get the wiring needed for our two inner networks
-            var permutationsInner =
-                GetInnerPermutations(permutation, delta, phase, out var inputMask, out var outputMask);
+            byte[][] permutationsInner = GetInnerPermutations(permutation, delta, phase, out inputMask, out outputMask);
 
             // Recursively create those two inner networks
             if (size == 4)
             {
                 // The recursion ends here by creating two swappers rather than normal permutation networks
-                permutationNetworkEven = new Swapper32(permutationsInner[0], phase);
-                permutationNetworkOdd = new Swapper32(permutationsInner[1], phase + delta);
+                permutationNetworkEven = new Swapper64(permutationsInner[0], phase);
+                permutationNetworkOdd = new Swapper64(permutationsInner[1], phase + delta);
             }
             else
             {
                 // For larger sizes, the inner networks are standard permutation networks
-                permutationNetworkEven = new PermutationNetwork32(permutationsInner[0], phase);
-                permutationNetworkOdd = new PermutationNetwork32(permutationsInner[1], phase + delta);
+                permutationNetworkEven = new PermutationNetwork64(permutationsInner[0], phase);
+                permutationNetworkOdd = new PermutationNetwork64(permutationsInner[1], phase + delta);
             }
 
             // Extract the masks from our inner permutation networks into our list of masks
             ExtractMasks(permutationNetworkEven, permutationNetworkOdd, inputMask, outputMask);
         }
 
-        private static byte[][] GetInnerPermutations(
-            byte[] permutationOuter,
-            int delta,
-            int phase,
-            out uint inputMask,
-            out uint outputMask)
+        private static byte[][] GetInnerPermutations(byte[] permutationOuter, int delta, int phase, out ulong inputMask,
+            out ulong outputMask)
         {
             inputMask = 0;
             outputMask = 0;
 
-            var permutationsInner = new byte[2][];
-            var size = permutationOuter.Length;
+            byte[][] permutationsInner = new byte[2][];
+            int size = permutationOuter.Length;
             permutationsInner[0] = new byte[size / 2];
             permutationsInner[1] = new byte[size / 2];
 
-            var reversePermutation = GetReversePermutation(permutationOuter);
+            byte[] reversePermutation = GetReversePermutation(permutationOuter);
 
             // We map out cycles until we've covered all the inputs and their
             // connections to the outputs.  mappedToDest keeps track of which
@@ -105,13 +100,13 @@ namespace ConsoleTests
             // mapped inputs.  When we end a cycle, if cMapped == size then
             // we're done.  If it's not, then we find an unmapped input and
             // map out the cycle starting from that input.
-            var mappedToDest = new bool[size];
-            var cMapped = 0;
+            bool[] mappedToDest = new bool[size];
+            int cMapped = 0;
 
             // While there are cycles to map...
             while (cMapped < size)
             {
-                var fInput = true;
+                bool fInput = true;
                 byte network = 0;
                 byte startPin;
 
@@ -125,15 +120,15 @@ namespace ConsoleTests
                 }
 
                 // Keep track of our starting pin
-                var cycleStartPin = startPin;
+                byte cycleStartPin = startPin;
 
                 // Adjacent pairs of pins are connected to a switcher which
                 // may or may not swap them around.  Get the index for the
                 // switcher we're attached to.
-                var switcher = (byte)(startPin / 2);
+                byte switcher = (byte)(startPin / 2);
 
                 // This is the pin we need to be mapped to
-                var endPin = permutationOuter[startPin];
+                byte endPin = permutationOuter[startPin];
 
                 // Trace the cycle starting at startPin
                 while (true)
@@ -142,7 +137,7 @@ namespace ConsoleTests
                     fInput = !fInput;
 
                     // Where we came from
-                    var switcherPrev = switcher;
+                    byte switcherPrev = switcher;
                     switcher = (byte)(endPin / 2);
 
                     // If we're connected to the wrong network currently...
@@ -192,8 +187,8 @@ namespace ConsoleTests
 
         private static byte[] GetReversePermutation(byte[] permutation)
         {
-            var size = permutation.Length;
-            var reversePermutation = new byte[size];
+            int size = permutation.Length;
+            byte[] reversePermutation = new byte[size];
             for (byte i = 0; i < size; i++)
             {
                 reversePermutation[permutation[i]] = i;
@@ -202,37 +197,22 @@ namespace ConsoleTests
             return reversePermutation;
         }
 
-        private static uint AddSwap(uint maskCur, int switcher, int delta, int phase)
+        private static ulong AddSwap(ulong maskCur, int switcher, int delta, int phase)
         {
-            return maskCur | (1u << (2 * switcher * delta + phase));
+            return maskCur | (1ul << (2 * switcher * delta + phase));
         }
 
-        private void ExtractMasks(PermutationNetwork32 even, PermutationNetwork32 odd, uint inputMask, uint outputMask)
+        private void ExtractMasks(PermutationNetwork64 even, PermutationNetwork64 odd, ulong inputMask, ulong outputMask)
         {
-            var masksEven = even.Masks;
-            var masksOdd = odd.Masks;
+            List<ulong> masksEven = even.Masks;
+            List<ulong> masksOdd = odd.Masks;
             Masks.Add(inputMask);
-            for (var i = 0; i < masksEven.Count; i++)
+            for (int i = 0; i < masksEven.Count; i++)
             {
                 Masks.Add(masksEven[i] | masksOdd[i]);
             }
 
             Masks.Add(outputMask);
-        }
-
-        private static readonly int[] Deltas = { 1, 2, 4, 8, 16, 8, 4, 2, 1 };
-
-        public uint Permute(uint value)
-        {
-            for (var i = 0; i < Deltas.Length; i++)
-            {
-                if (Masks[i] != 0)
-                {
-                    value = Bitops.SwapBitsMask(value, Deltas[i], Masks[i]);
-                }
-            }
-
-            return value;
         }
     }
 }
