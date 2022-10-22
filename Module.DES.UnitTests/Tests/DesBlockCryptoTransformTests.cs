b1ca65c4acc0c1c799b486a1efe557203aded42d
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 17:29:09 +0300

    Add tests for DES block crypto transforms

---
+++ Module.DES.UnitTests/Tests/DesBlockCryptoTransformTests.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Module.Core.Factories.Abstract;
+using Module.Core.UnitTests.Tests;
+using Module.DES.Entities;
+using Module.DES.Entities.Abstract;
+using NUnit.Framework;
+
+namespace Module.DES.UnitTests.Tests;
+
+[TestFixture]
+public class DesBlockCryptoTransformTests : BlockCryptoTransformBaseTests<IDesParameters>
+{
+    private readonly IContainer _container;
+
+    public DesBlockCryptoTransformTests()
+    {
+        _container = BuildContainer();
+    }
+
+    [Test]
+    public void Transform_Test()
+    {
+        TestTransform(GetDefaultParameters());
+    }
+
+    protected override IBlockCryptoTransformFactory<IDesParameters> GetBlockCryptoTransformFactory()
+    {
+        return _container.Resolve<IBlockCryptoTransformFactory<IDesParameters>>();
+    }
+
+    protected override IDesParameters GetDefaultParameters()
+    {
+        return new DesParameters(0b00111100_11011101_10110010_01111001_01011011_00000110_10110110);
+    }
+
+    private static IContainer BuildContainer()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterModule<DesModule>();
+
+        return builder.Build();
+    }
+}
