d60f0cf7c816f0f0c3273e097167d9e6190bf05d
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 13:32:50 +0300

    Add realisation of BitOperationsService

---
+++ Module.DES/Services/BitOperationsService.cs
@@ -0,0 +1,16 @@
+﻿using Module.DES.Services.Abstract;
+
+namespace Module.DES.Services;
+
+public class BitOperationsService : IBitOperationsService
+{
+    public byte XorBits(byte value)
+    {
+        var result = value;
+        result = (byte)(result ^ (result >> 4));
+        result = (byte)(result ^ (result >> 2));
+        result = (byte)(result ^ (result >> 1));
+
+        return (byte)(result & 1);
+    }
+}
