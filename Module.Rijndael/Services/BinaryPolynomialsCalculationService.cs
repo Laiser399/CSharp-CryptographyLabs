cbcc57c68cd048c2327b2091b10dda300a1e737e
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 21:35:24 +0300

    Add implementation of binary polynomials multiplication

--- Module.Rijndael/Services/BinaryPolynomialsCalculationService.cs
+++ Module.Rijndael/Services/BinaryPolynomialsCalculationService.cs
@@ -6,6 +6,19 @@ public class BinaryPolynomialsCalculationService : IBinaryPolynomialsCalculation
 {
     public ulong Multiply(uint a, uint b)
     {
-        throw new NotImplementedException();
+        var bExt = (ulong)b;
+        var result = 0ul;
+        while (a > 0)
+        {
+            if ((a & 1) == 1)
+            {
+                result ^= bExt;
+            }
+
+            a >>= 1;
+            bExt <<= 1;
+        }
+
+        return result;
     }
 }
