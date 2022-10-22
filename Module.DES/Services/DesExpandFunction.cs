4b39968085087f9cf3a9367b9bf668eae88cb03d
Sergey Semenov <Laiser399@mail.ru>
2022-10-22 13:01:05 +0300

    Add realisation of DES expand function

--- Module.DES/Services/DesExpandFunction.cs
+++ Module.DES/Services/DesExpandFunction.cs
@@ -6,6 +6,16 @@ public class DesExpandFunction : IDesExpandFunction
 {
     public ulong Calculate(uint value)
     {
-        throw new NotImplementedException();
+        ulong result = 0;
+
+        value = (value << 5) | (value >> 27);
+        for (var i = 0; i < 8; ++i)
+        {
+            result = (result << 6) | (value & 0b111111);
+
+            value = (value << 4) | (value >> 28);
+        }
+
+        return result;
     }
 }
