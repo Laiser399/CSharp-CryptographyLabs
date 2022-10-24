69c5939d131ea5e4c8776fe0e31a52a434b4c611
Sergey Semenov <Laiser399@mail.ru>
2022-10-25 00:27:10 +0300

    Add program realisation of Wiener attack testing

---
+++ Util.RSA.WienerAttackTest/AppContainer.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Features.AttributeFilters;
+using Module.RSA;
+using Util.RSA.WienerAttackTest.Services;
+using Util.RSA.WienerAttackTest.Services.Abstract;
+
+namespace Util.RSA.WienerAttackTest;
+
+public static class AppContainer
+{
+    public static IContainer Build()
+    {
+        var builder = new ContainerBuilder();
+
+        builder
+            .RegisterType<WienerAttackTestService>()
+            .As<IWienerAttackTestService>()
+            .WithAttributeFiltering();
+
+        builder.RegisterModule(new RSAModule
+        {
+            RegisterRsaCore = true,
+            RegisterPrimesGenerator = true,
+            RegisterRsaKeyGenerator = true,
+            RegisterAttackServices = true,
+        });
+
+        return builder.Build();
+    }
+}
--- Util.RSA.WienerAttackTest/Program.cs
+++ Util.RSA.WienerAttackTest/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Autofac;
+using Module.RSA.Entities;
+using Module.RSA.Entities.Abstract;
+using Util.RSA.WienerAttackTest;
+using Util.RSA.WienerAttackTest.Entities;
+using Util.RSA.WienerAttackTest.Entities.Abstract;
+using Util.RSA.WienerAttackTest.Services.Abstract;
 
-Console.WriteLine("Hello, World!");
+var container = AppContainer.Build();
+
+var scope = container.BeginLifetimeScope(builder =>
+{
+    builder
+        .RegisterInstance(new RandomProvider(new Random()))
+        .As<IRandomProvider>();
+    builder
+        .RegisterInstance(new PrimalityTesterParameters(0.999))
+        .As<IPrimalityTesterParameters>();
+});
+
+var parameters = new WienerAttackTestServiceParameters(
+    Enumerable.Range(1, 8).Select(x => x * 16),
+    10
+);
+
+var wienerAttackTestService = scope.Resolve<IWienerAttackTestService>(
+    new TypedParameter(typeof(IWienerAttackTestServiceParameters), parameters)
+);
+
+var results = await wienerAttackTestService.PerformComplexTestAsync();
