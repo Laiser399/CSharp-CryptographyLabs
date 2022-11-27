c2bb43cab6e92528ba179f36793363aacc6dddd2
Sergey Semenov <Laiser399@mail.ru>
2022-11-27 23:52:22 +0300

    Add GUI component for Galois field's element representation task

---
+++ CryptographyLabs/GUI/Views/GaloisFieldElementRepresentation.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class GaloisFieldElementRepresentation : Grid
+{
+    public GaloisFieldElementRepresentation()
+    {
+        InitializeComponent();
+    }
+}
