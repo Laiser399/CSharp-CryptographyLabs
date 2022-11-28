d8dc024b5a120f4e3fc3827b7caf367cd4e2f605
Sergey Semenov <Laiser399@mail.ru>
2022-11-28 22:30:54 +0300

    Add interface for GF elements multiplication view model; Add appropriate bindings.

---
+++ CryptographyLabs/GUI/AbstractViewModels/IGaloisFieldElementsMultiplicationVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IGaloisFieldElementsMultiplicationVM
+{
+    string GeneratingElement { get; set; }
+    string First { get; set; }
+    string Second { get; set; }
+    string MultiplicationResult { get; }
+
+    string CalculatedGeneratingElements { get; }
+
+    ICommand Multiply { get; }
+    ICommand CalculateGeneratingElements { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/GaloisFieldElementsMultiplicationDTVM.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class GaloisFieldElementsMultiplicationDTVM : IGaloisFieldElementsMultiplicationVM
+{
+    public string GeneratingElement { get; set; } = "0b100011011";
+    public string First { get; set; } = "0b1001101";
+    public string Second { get; set; } = "0b1100110";
+    public string MultiplicationResult => "0b10000";
+
+    public string CalculatedGeneratingElements => "First generating element\nSecond generating element";
+
+    public ICommand Multiply => RelayCommand.Empty;
+    public ICommand CalculateGeneratingElements => RelayCommand.Empty;
+}
