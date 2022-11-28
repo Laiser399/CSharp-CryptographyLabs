33e5b456107fc7156beef4c657e9e6e23f337616
Sergey Semenov <Laiser399@mail.ru>
2022-11-28 18:35:07 +0300

    Add interface for binary polynomial representation

---
+++ Module.Rijndael/Services/Abstract/IBinaryPolynomialRepresentationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Module.Rijndael.Services.Abstract;
+
+public interface IBinaryPolynomialRepresentationService
+{
+    string ToStringAsPolynomial(uint value);
+
+    bool TryParseAsPolynomial(string polynomial, out uint value);
+}
