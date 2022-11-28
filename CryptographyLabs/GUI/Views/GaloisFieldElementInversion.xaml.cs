10f18f1fee02f0fae944d0faa0d7f72032ec3fe6
Sergey Semenov <Laiser399@mail.ru>
2022-11-28 23:26:31 +0300

    Add view for GF element inversion task

---
+++ CryptographyLabs/GUI/Views/GaloisFieldElementInversion.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class GaloisFieldElementInversion : Grid
+{
+    public GaloisFieldElementInversion()
+    {
+        InitializeComponent();
+    }
+}
