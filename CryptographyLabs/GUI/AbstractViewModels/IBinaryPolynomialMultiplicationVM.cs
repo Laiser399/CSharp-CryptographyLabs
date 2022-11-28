111d78c4de094be7f4f2f1fb81375bbe68952681
Sergey Semenov <Laiser399@mail.ru>
2022-11-28 19:07:38 +0300

    Add design time view model for binary polynomial multiplication; Add appropriate bindings.

---
+++ CryptographyLabs/GUI/AbstractViewModels/IBinaryPolynomialMultiplicationVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IBinaryPolynomialMultiplicationVM
+{
+    string FirstPolynomial { get; set; }
+    string SecondPolynomial { get; set; }
+    string MultiplicationResult { get; }
+
+    ICommand Multiply { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/BinaryPolynomialMultiplicationDTVM.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class BinaryPolynomialMultiplicationDTVM : IBinaryPolynomialMultiplicationVM
+{
+    public string FirstPolynomial { get; set; } = "x^7 + x + 1";
+    public string SecondPolynomial { get; set; } = "x^31 + x^3";
+    public string MultiplicationResult => "x^38 + x^32 + x^31 + x^10 + x^4 + x^3";
+
+    public ICommand Multiply => RelayCommand.Empty;
+}
