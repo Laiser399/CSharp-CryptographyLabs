d6176fe495187486099152c8973668347ecd1f79
Sergey Semenov <Laiser399@mail.ru>
2022-11-28 18:44:47 +0300

    Add view component for binary polynomials multiplication task

---
+++ CryptographyLabs/GUI/Views/BinaryPolynomialMultiplication.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptographyLabs.GUI.Views;
+
+public partial class BinaryPolynomialMultiplication : Grid
+{
+    public BinaryPolynomialMultiplication()
+    {
+        InitializeComponent();
+    }
+}
