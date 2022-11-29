b76d0c5708c39bcece7dd8d0a4c70fb08f9130b0
Sergey Semenov <Laiser399@mail.ru>
2022-11-29 13:44:00 +0300

    Add view model and bindings to GF element inversion

---
+++ CryptographyLabs/GUI/AbstractViewModels/IGaloisFieldElementInversionVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IGaloisFieldElementInversionVM
+{
+    string InputValue { get; set; }
+    string InversedValue { get; }
+
+    ICommand Inverse { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/GaloisFieldElementInversionDTVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class GaloisFieldElementInversionDTVM : IGaloisFieldElementInversionVM
+{
+    public string InputValue { get; set; } = "0";
+    public string InversedValue => "0";
+
+    public ICommand Inverse => RelayCommand.Empty;
+}
