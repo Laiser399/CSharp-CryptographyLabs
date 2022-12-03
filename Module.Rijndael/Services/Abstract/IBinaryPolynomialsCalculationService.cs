840407265c0fa4e3e1a34c0ff4e4c0c80b0ee472
Sergey Semenov <Laiser399@mail.ru>
2022-12-04 02:00:39 +0300

    Add realisation of Extended Euclidean algorithm for binary polynomials

--- Module.Rijndael/Services/Abstract/IBinaryPolynomialsCalculationService.cs
+++ Module.Rijndael/Services/Abstract/IBinaryPolynomialsCalculationService.cs
@@ -3,4 +3,11 @@
 public interface IBinaryPolynomialsCalculationService
 {
     ulong Multiply(uint a, uint b);
+
+    uint Divide(uint a, uint b, out uint residual);
+
+    /// <summary>
+    /// Вычисляет и возвращает НОД, а за одно и коэффициенты такие, что НОД = x * a + y * b.
+    /// </summary>
+    uint GreatestCommonDivisor(uint a, uint b, out uint x, out uint y);
 }
--- Module.Rijndael/Services/BinaryPolynomialsCalculationService.cs
+++ Module.Rijndael/Services/BinaryPolynomialsCalculationService.cs
@@ -1,4 +1,5 @@
-﻿using Module.Rijndael.Services.Abstract;
+﻿using Module.Core.Extensions;
+using Module.Rijndael.Services.Abstract;
 
 namespace Module.Rijndael.Services;
 
@@ -20,5 +21,47 @@ public class BinaryPolynomialsCalculationService : IBinaryPolynomialsCalculation
         }
 
         return result;
+    }
+
+    public uint Divide(uint a, uint b, out uint residual)
+    {
+        var result = Divide(a, b, out ulong residualTemp);
+        residual = (uint)residualTemp;
+        return (uint)result;
+    }
+
+    private static ulong Divide(ulong a, ulong b, out ulong residual)
+    {
+        var aBitLength = a.GetBitLength();
+        var bBitLength = b.GetBitLength();
+
+        ulong result = 0;
+
+        while (aBitLength >= bBitLength)
+        {
+            var diff = aBitLength - bBitLength;
+            a ^= b << diff;
+            result ^= 1ul << diff;
+            aBitLength = a.GetBitLength();
+        }
+
+        residual = a;
+        return result;
+    }
+
+    public uint GreatestCommonDivisor(uint a, uint b, out uint x, out uint y)
+    {
+        (x, y) = (1, 0);
+        var (xCurrent, yCurrent) = (0u, 1u);
+
+        while (b > 0)
+        {
+            var q = Divide(a, b, out var residual);
+            (a, b) = (b, residual);
+            (xCurrent, x) = ((uint)(x ^ Multiply(q, xCurrent)), xCurrent);
+            (yCurrent, y) = ((uint)(y ^ Multiply(q, yCurrent)), yCurrent);
+        }
+
+        return a;
     }
 }
