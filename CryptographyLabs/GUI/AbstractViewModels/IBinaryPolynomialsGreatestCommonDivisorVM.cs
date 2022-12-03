c49dba0e33c5695117e1ac74a5886c62388a30a9
Sergey Semenov <Laiser399@mail.ru>
2022-12-04 02:18:49 +0300

    Add abstract view model and bindings for binary polynomials Extended Euclidean algorithm view

---
+++ CryptographyLabs/GUI/AbstractViewModels/IBinaryPolynomialsGreatestCommonDivisorVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+
+namespace CryptographyLabs.GUI.AbstractViewModels;
+
+public interface IBinaryPolynomialsGreatestCommonDivisorVM
+{
+    string A { get; set; }
+    string B { get; set; }
+
+    string X { get; }
+    string Y { get; }
+    string GreatestCommonDivisor { get; }
+
+    ICommand Calculate { get; }
+}
---
+++ CryptographyLabs/GUI/DesignTimeViewModel/BinaryPolynomialsGreatestCommonDivisorDTVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using CryptographyLabs.GUI.AbstractViewModels;
+
+namespace CryptographyLabs.GUI.DesignTimeViewModel;
+
+public class BinaryPolynomialsGreatestCommonDivisorDTVM : IBinaryPolynomialsGreatestCommonDivisorVM
+{
+    public string A { get; set; } = "0b101";
+    public string B { get; set; } = "0b1001";
+
+    public string X => "0b10";
+    public string Y => "0b1";
+    public string GreatestCommonDivisor => "0b11";
+
+    public ICommand Calculate => RelayCommand.Empty;
+}
